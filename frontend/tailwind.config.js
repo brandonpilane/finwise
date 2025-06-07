3732deef5e76e6f0afc96df4e4f775ab6ef31be9
Brandon <BrandonPilane@gmail.com>
2025-06-07 17:48:58 +0200

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Crucial: Includes .js and .jsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
